02e4f8ced43bf9104ed485dcb4fe39a08e6f414b
Vinsensius Arden <zeroxleashgaming@gmail.com>
2025-02-05 16:15:55 +0700

    Pesan commit baru

--- app/src/main/java/com/example/modul11/MainActivity.kt
+++ app/src/main/java/com/example/modul11/MainActivity.kt
@@ -3,33 +3,27 @@ package com.example.modul11
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.codelab.basics.ui.theme.BasicsCodelabTheme
 import com.example.modul11.ui.theme.Modul11Theme
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Modul11Theme  {
+            Modul11Theme {
                 MyApp(modifier = Modifier.fillMaxSize())
             }
         }
@@ -38,8 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if (shouldShowOnboarding) {
@@ -55,7 +48,6 @@ fun OnboardingScreen(
     onContinueClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -72,31 +64,28 @@ fun OnboardingScreen(
 }
 
 @Composable
-private fun Greetings(
+fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "User $it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(names) { name ->
             Greeting(name = name)
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    Modul11Theme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
     var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -111,19 +100,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            ElevatedButton(
-                onClick = { expanded = !expanded }
-            ) {
+            ElevatedButton(onClick = { expanded = !expanded }) {
                 Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    Modul11Theme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
-    Modul11Theme{
+    Modul11Theme {
         Greetings()
     }
 }
